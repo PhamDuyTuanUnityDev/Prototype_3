945fbf54e5d3d3c39cb550ef463de369e1e85ccc
PhamDuyTuanUnityDev <109053805+PhamDuyTuanUnityDev@users.noreply.github.com>
2022-07-11 16:44:45 +0700

    Initial commit
    Lesson 3.4 - Particles and Sound Effects

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,25 +10,34 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool gameOver = false;
     private Animator playerAnim;
+    public ParticleSystem exParticleSystem;
+    public ParticleSystem dirtParticle;
+    private AudioSource playerAudio;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
     private void Start()
     {
         playerRb = GetComponent<Rigidbody>();
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
         Physics2D.gravity *= gravityModifier;
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && gameOver)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             playerAnim.SetTrigger("Jump_trig");
             isOnGround = false;
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(jumpSound, 1.0f);
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Ground")
         {
+            dirtParticle.Play();
             isOnGround = true;
         }
         else if (collision.gameObject.tag == "Obstacle")
@@ -37,6 +46,9 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
             Debug.Log("GameOver");
+            exParticleSystem.Play();
+            dirtParticle.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
     }
 }
